de62b9199841bbac31f8da4657bc9edf0c4e38fa
mayconcad <maycon@tecnodocs.com.br>
2017-02-14 21:07:37 -0300

    teste

--- src/main/java/br/com/asconp/asconp_transparencia/vos/BaseVO.java
+++
@@ -1,9 +0,0 @@
-package br.com.asconp.asconp_transparencia.vos;
-
-import java.io.Serializable;
-
-public abstract class BaseVO implements Serializable {
-
-	private static final long serialVersionUID = 5928960412643307406L;
-
-}
